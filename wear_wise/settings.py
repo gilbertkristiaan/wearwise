55b6df093d2a30723995ef7ac763a0e11ae91598
gilbertkristiaan <gilbertkristian79@gmail.com>
2024-09-09 15:07:37 +0700

    tugas 2  pbp (revisi 2) selesai

--- wear_wise/settings.py
+++ wear_wise/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$bi%47#0-s5&=e@w34s+9u*!i1-3k)f71)vo=zbva=-33h%^$1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "gilbert-kristian-wearwise.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "gilbert-kristian-wearwise.pbp.cs.ui.ac.id/"]
 
 
 # Application definition
