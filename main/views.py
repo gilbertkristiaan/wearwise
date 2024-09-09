a6e15b910da047c051e116f7c64f5a774d71c22e
gilbertkristiaan <gilbertkristian79@gmail.com>
2024-09-09 15:25:02 +0700

    ganti html dan views.py

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ def show_main(request):
         'myname': 'Gilbert Kristian',
         'kelas': 'PBP D',
         'name' : 'Wearwise Black T-Shirt',
-        'price': 'Rp150.000',
+        'price': 150000,
         'description': 'Baju hitam ini adalah pilihan sempurna untuk gaya yang sederhana namun elegan. Terbuat dari bahan berkualitas tinggi, baju ini memberikan kenyamanan maksimal sepanjang hari. Dengan desain yang klasik dan potongan yang pas, baju ini cocok untuk berbagai kesempatan, baik formal maupun santai.',
         'quantity' : 300
     }
