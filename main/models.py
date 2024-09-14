0429b7bd002fe90e64fc8cbb40009f7a9c8148e2
gilbertkristiaan <gilbertkristian79@gmail.com>
2024-09-14 13:09:10 +0700

    tugas 3 (in-work)

--- main/models.py
+++ main/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+import uuid
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
-    quantity = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=1)
 
     @property
     def __str__(self):
--- wear_wise/settings.py
+++ wear_wise/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'wear_wise.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
