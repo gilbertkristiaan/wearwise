8307cc6ee0a833b32df2187f0933cd6da6033b02
gilbertkristiaan <gilbertkristian79@gmail.com>
2024-09-18 10:27:06 +0700

    coba deploy ulang

--- wear_wise/settings.py
+++ wear_wise/settings.py
@@ -123,3 +123,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://gilbert-kristian-wearwise.pbp.cs.ui.ac.id/", "https://gilbert-kristian-wearwise.pbp.cs.ui.ac.id/"]
+#
